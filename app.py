4621802c1fcf4edffdc438dd62d9e61b5d54a00f
Daopz <ingdoliveros@gmail.com>
2021-04-06 11:06:46 -0300

    Second commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import os
-from aws_cdk import core as cdk
+
 from aws_cdk import core
 from python_greenline.python_greenline_stack import PythonGreenlineStack
 from python_greenline.pipeline_stack import PipelineStack
@@ -8,7 +7,7 @@ from python_greenline.pipeline_stack import PipelineStack
 PIPELINE_ACCOUNT = '507856266964'
 
 app = core.App()
-PythonGreenlineStack(app, "PythonGreenlineStack")
+
 
 PythonGreenlineStack(app, "PipelineStack", env={
   'account': PIPELINE_ACCOUNT,
--- python_greenline/pipeline_stack.py
+++ python_greenline/pipeline_stack.py
@@ -8,26 +8,27 @@ from aws_cdk import pipelines
 #APP_ACCOUNT = '507856266964'
 
 class PipelineStack(core.Stack):
-  def __init__(self, scope: core.Construct, id: str, **kwargs):
-    super().__init__(scope, id, **kwargs)
 
-    source_artifact = codepipeline.Artifact()
-    cloud_assembly_artifact = codepipeline.Artifact()
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
 
-    pipelines.CdkPipeline(self, 'Pipeline',
-      cloud_assembly_artifact=cloud_assembly_artifact,
-      pipeline_name='GrennlinePipeline',
+        source_artifact = codepipeline.Artifact()
+        cloud_assembly_artifact = codepipeline.Artifact()
 
-      source_action=cpactions.GitHubSourceAction(
-        action_name='GitHub',
-        output=source_artifact,
-        oauth_token=core.SecretValue.secrets_manager('python_token'),
-        owner='daopz',
-        repo='python_function',
-        trigger=cpactions.GitHubTrigger.POLL),
-
-      synth_action=pipelines.SimpleSynthAction(
-        source_artifact=source_artifact,
+        pipelines.CdkPipeline(self, 'Pipeline',
         cloud_assembly_artifact=cloud_assembly_artifact,
-        install_command='npm install -g aws-cdk && pip install -r requirements.txt',
-        synth_command='cdk synth'))
+        pipeline_name='GrennlinePipeline',
+
+        source_action=cpactions.GitHubSourceAction(
+            action_name='GitHub',
+            output=source_artifact,
+            oauth_token=core.SecretValue.secrets_manager('python_token'),
+            owner='daopz',
+            repo='python_function',
+            trigger=cpactions.GitHubTrigger.POLL),
+
+        synth_action=pipelines.SimpleSynthAction(
+            source_artifact=source_artifact,
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            install_command='npm install -g aws-cdk && pip install -r requirements.txt',
+            synth_command='cdk synth'))
